2692acc8825a7a9488ea30c866a35f6a1f5f5a4e
Doose Deborah Sen <doosedear@gmail.com>
2025-02-23 07:44:57 +0100

    refactored UserProfile.jsx to use context API instead of props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-const UserProfile = (props) => {
-    return ( 
-        <div className="userprofile">
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-     );
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+const UserProfile = () => {
+  const user = useContext(UserContext); // Accessing user data from context
+
+  return ( 
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
+      <h2 style={{ color: 'blue' }}>{user.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{user.age}</span></p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
 }
- 
+
 export default UserProfile;
