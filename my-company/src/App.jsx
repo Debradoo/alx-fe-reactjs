55e9456645edf6353d67bbf16f1d60e15ad15d82
Doose Deborah Sen <doosedear@gmail.com>
2025-02-23 09:06:26 +0100

    Added Navbar Component for easy navigation

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,13 +3,16 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   
 
   return (
     <>
+     <Navbar />
      <Routes>
+      
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
