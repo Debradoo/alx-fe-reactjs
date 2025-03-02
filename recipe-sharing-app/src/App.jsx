38c03111549ec1032bd5bf6bda890532d85eb21a
Doose Deborah Sen <doosedear@gmail.com>
2025-03-02 11:07:40 +0100

    Add RecipeDetails, EditRecipeForm, and DeleteRecipeButton components with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   
 
   return (
     <>
-      <AddRecipeForm/>
-      <RecipeList/>
+      <Router>
+      <Routes>
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/" element={<AddRecipeForm />} />
+        <Route path="/" element={<RecipeList />} />
+        
+
+      </Routes>
+    </Router>
     </>
   )
 }
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,28 @@
-import create from 'zustand'
+import create from 'zustand';
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Delete a recipe by ID
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+
+  // Update a recipe by ID
+  updateRecipe: (recipeId, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  // Set the recipe list (initialization)
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
