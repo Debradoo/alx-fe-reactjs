662ae30aa0a2053f60f158c8935472efa942746d
Doose Deborah Sen <doosedear@gmail.com>
2025-03-13 13:16:54 +0100

    did some modification in my program

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,20 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+// Function to fetch posts
+const fetchPosts = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return response.data;
+};
+
 function PostsComponent() {
   // Fetch posts using React Query
-  const { data, error, isLoading, refetch } = useQuery({
+  const { data, isError, isLoading, refetch } = useQuery({
     queryKey: ["posts"],
-    queryFn: async () => {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      return response.data;
-    },
+    queryFn: fetchPosts,
     staleTime: 5000, // Data remains fresh for 5 seconds
     cacheTime: 300000, // Cache persists for 5 minutes
   });
 
   if (isLoading) return <p>Loading posts...</p>;
-  if (error) return <p>Error loading posts: {error.message}</p>;
+  if (isError) return <p>Error loading posts</p>;
 
   return (
     <div>
