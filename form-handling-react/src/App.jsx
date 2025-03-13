50de51bc848cf6046d0ab2a0182affabfe75b864
Doose Deborah Sen <doosedear@gmail.com>
2025-03-13 11:50:04 +0100

    added a form-handling-react app

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import FormikForm from "./components/formikForm";
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      
+      <FormikForm />
+    </>
+  )
+}
+
+export default App
