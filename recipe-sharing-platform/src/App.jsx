b2c3e5208768fc42fff4ee38271ca4b7783f1cb3
Doose Deborah Sen <doosedear@gmail.com>
2025-03-21 23:26:28 +0100

    Add Recipe Detail Page with Dynamic Routing & Tailwind Styling

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
-
 import HomePage from "./components/HomePage";
 
 function App() {
  
 
   return (
-    <>
-    <div className="bg-gray-100 min-h-screen">
-      <HomePage />
-    </div>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/recipe/:id" element={<RecipeDetail />} />
+    </Routes>
+  </Router>
   )
 }
 
